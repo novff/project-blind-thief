be0a43f2b995715021ac1f641354e55494c21dc0
novff <max20042011@gmail.com>
2021-01-04 22:37:03 +0300

    whole lotta crap

---
+++ Assets/scripts/MovementBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MovementBehaviourScript : MonoBehaviour
+{
+    void Start() {}
+
+    void MovementCheck ()
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
+                {
+                    
+                }
+
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+
+                }
+
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    
+                }
+
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    
+                }
+        }
+
+    // Update is called once per frame
+    void Update()
+        {
+          MovementCheck ();
+        }
+}
