181f73b2421cad5c00390d9807700c2c8e7f0b76
novff <max20042011@gmail.com>
2021-03-01 19:53:51 +0300

    even more collision tests
    i have an idea to move it to coroutine with a coroutine inside but not now

--- Assets/scripts/MovementBehaviourScript.cs
+++ Assets/scripts/MovementBehaviourScript.cs
@@ -8,9 +8,9 @@ using UnityEngine;
 
 public class MovementBehaviourScript : MonoBehaviour
 {
+//smooth movement coroutine and variables
     bool IsColliding = false;
-
-    public Vector3 PlayerOgPos, PlayerTrgtPos;
+    public Vector3 PlayerOgPos, PlayerTrgtPos, RcColDir;
     private float MovTime = 0.2F;
     private bool isMoving = false;
     private IEnumerator MovePlayer (Vector3 direction)
@@ -27,16 +27,12 @@ public class MovementBehaviourScript : MonoBehaviour
                             ElTime += Time.deltaTime;
                             yield return null;
                         }
+//transition transform to fix imperfect position after smooth movement while cycle
                     transform.position = PlayerTrgtPos;
                     isMoving = false;
                 }
-            else
-                {
-                    IsColliding = false;
-                }
-
         }
-
+//smooth rotation coroutine and variables
     public int FakeAngle = 0;
     private float RotTime = 0.4F;
     private bool isRotating = false;
@@ -53,11 +49,13 @@ public class MovementBehaviourScript : MonoBehaviour
                     ElTime += Time.deltaTime;
                     yield return null;
                 }
-                //NOTE: NEED TO FIX IMEPRFECT ROTATION
+                //NOTE: NEED TO FIX IMEPRFECT ROTATION afaik comment bellow doesnt work 
                 //transform.rotation *= Quaternion.AngleAxis(rotationY, Vector3.up);
             isRotating = false;
         }
-
+//lines  a and d start rotation coroutine and also change fake angles for tank like movement
+//i could use switch cases for fake angle check in movement but im to lazy to do that and there's no way im rewriting this shit
+//fake angle is a workaround for afaik no way to get transfrom.rotation in a useable way
     void MovementCheck ()
         {
             if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && isMoving == false) 
@@ -83,7 +81,6 @@ public class MovementBehaviourScript : MonoBehaviour
                             StartCoroutine(MovePlayer(Vector3.back));
                         } 
                 }
-
             if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && isRotating == false)
                 {
                     FakeAngle = FakeAngle -90;
@@ -99,6 +96,7 @@ public class MovementBehaviourScript : MonoBehaviour
 
             if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && isMoving == false)
                 {
+
                     if (FakeAngle == 0)
                         {
                             StartCoroutine(MovePlayer(Vector3.back));
@@ -122,10 +120,63 @@ public class MovementBehaviourScript : MonoBehaviour
                     
                 }
         }
+//im using raycast collision check because unity rigidbody is a gimmickful piece of shit which cant really be used for movement prevention
+    void CollisionCheck()
+        {
+            RaycastHit hit;
+            Ray ColRay = new Ray(PlayerOgPos,  RcColDir);
+            Ray ColRayBack = new Ray(PlayerOgPos,  -RcColDir);
+            if (Physics.Raycast(ColRay, out hit, 0.5F))
+                {
+                    if (hit.collider.gameObject.name == "Wall(Clone)")
+                        {
+                            IsColliding = true;
+                        }
+                    else
+                        {
+                            IsColliding = false;
+                        }
+                }
+            if (Physics.Raycast(ColRayBack, out hit, 0.5F))
+                {
+                    if (hit.collider.gameObject.name == "Wall(Clone)")
+                        {
+                            IsColliding = true;
+                        }
+                    else
+                        {
+                            IsColliding = false;
+                        }
+                }
+        }
+//this one updates every frame and calls for movement voids
     void Update()
         {
+//raycast collision attached way to rotate the ray along with the player instance
+            if (FakeAngle == 0)
+                    {
+                        RcColDir = Vector3.forward;
+                    } 
+            if (FakeAngle == 90)
+                    {
+                        RcColDir = Vector3.right;
+                    }
+            if (FakeAngle == -90)
+                    {
+                        RcColDir = Vector3.left;
+                    } 
+                if (FakeAngle == 180)
+                    {
+                        RcColDir = Vector3.back;  
+                    } 
+                if (FakeAngle == -180)
+                    {
+                        RcColDir = Vector3.back; 
+                    }
+//using void calls to make it easier
+            CollisionCheck ();
             MovementCheck ();
-            
+//fake angle reset for better management
             if (FakeAngle == 270)
                 {
                     FakeAngle = -90;
@@ -137,22 +188,4 @@ public class MovementBehaviourScript : MonoBehaviour
                 }
 
         }
-    void Start()
-        {
-        }
-    
-    void OnCollisionEnter(Collision collision)
-        {
-            if(collision.gameObject.tag == "wall")
-                {
-                    
-                    IsColliding = true;
-                    UnityEngine.Debug.Log("Do something here");
-                }
-            else
-                {
-                    IsColliding = false;
-                }
-        }
-    
 }
