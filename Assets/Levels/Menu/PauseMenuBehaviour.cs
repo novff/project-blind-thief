37a9bc4cb62ce74dc6ebd776ff139bdec6c3c232
novff <max20042011@gmail.com>
2021-04-18 21:05:33 +0300

    pause menu and stuff

---
+++ Assets/Levels/Menu/PauseMenuBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuBehaviour : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject PauseUI;
+    void Pause()
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0f;
+            IsPaused = true;
+        }
+    public void Resume()
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+            IsPaused = false;
+        }
+    public void Restart()
+        {
+        }
+    public void ToMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+    void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    if (IsPaused)
+                        {
+                            Resume();
+                        }
+                    else
+                        {
+                            Pause();
+                        }
+                }
+        }
+}
